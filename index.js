f0ce0fb263a2bcfd7a11566dc092f04327c24136
Kyrian <dr.kyrian.onwe@gmail.com>
2021-10-15 19:03:33 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(trip){
   // Write your code here!
+  
+  if (trip <= 400){
+    return "This one is on me!"
+  }else if(trip > 2000 && trip < 2500){
+    return "I will gladly take your thirty bucks."
+  }else if(trip > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isNYC = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return isNYC;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ 
+
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much."; 
+      return response;
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      return response;
+      break;
+    default:
+      response = 'Bye.';
+      return response;
+      break;  
+        
+  }
 }
