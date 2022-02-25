603c74d9ce5c3bcae2a6f0ec5d23a684357176dc
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-24 23:27:42 -0600

    commit tree 65f38897f095659fc907ebcab2b0918d78ab6961

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(trip){
+/*function scuberGreetingForFeet(trip){
   // Write your code here!
   
   if (trip <= 400){
@@ -36,5 +36,37 @@ function switchOnCharmFromTip(tip){
       return response;
       break;  
         
+  }
+}*/
+
+function scuberGreetingForFeet(ride){
+  if (ride > 2500){
+    return 'No can do.'
+  } else if(ride > 2000){
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride <= 400){
+    return 'This one is on me!'
+  }
+}
+
+function ternaryCheckCity(city){
+  const isNYC = city === 'NYC'? 'Ok, sounds good.':'No go.';
+  return isNYC;
+}
+
+function switchOnCharmFromTip(tip){
+  let response;
+
+  switch (tip){
+    case 'generous':
+      return response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return response = 'Thank you.';
+      break;
+    default:
+      return response = 'Bye.';
+      break;
+
   }
 }
